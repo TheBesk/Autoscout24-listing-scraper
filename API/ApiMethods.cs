85519d4bd4fe6913a1972ea980f510f5c0e39667
TheBesk <46002209+TheBesk@users.noreply.github.com>
2024-10-28 20:48:04 +0100

    Aggiungere i file di progetto.

---
+++ API/ApiMethods.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autoscout24_listing_scraper.Model;
+using System.Text.Json;
+
+namespace Autoscout24_listing_scraper.API
+{
+    public class ApiMethods
+    {
+        public List<Listing> GetListings()
+        {
+            Rootobject? listings = new();
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://www.autoscout24.it/_next/data/as24-search-funnel_main-445/lst.json");
+                client.DefaultRequestHeaders.Add("sort", "standard");
+                client.DefaultRequestHeaders.Add("desc", "0");
+                client.DefaultRequestHeaders.Add("priceto", "5000");
+                HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, client.BaseAddress);
+                var response = client.Send(request);
+                listings = JsonSerializer.Deserialize<Rootobject>(response.Content.ReadAsStream());
+                
+            }
+            return listings.pageProps.listings.ToList();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿using Autoscout24_listing_scraper.API;
+using Autoscout24_listing_scraper.Model;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+ApiMethods api = new();
+List<Listing> test = api.GetListings();
